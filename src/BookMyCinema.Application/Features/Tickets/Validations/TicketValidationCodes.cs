72391686c3e75285e1e3403d2b3e5abc43a8bb8f
Ahmed Fawzi <ahmedfawzi636@gmail.com>
2026-04-07 19:25:35 +0200

    Add ValidationCodes static constant classes example

---
+++ src/BookMyCinema.Application/Features/Tickets/Validations/TicketValidationCodes.cs
@@ -0,0 +1,6 @@
+namespace BookMyCinema.Application.Features.Tickets.Validations;
+internal static class TicketValidationCodes
+{
+    internal const string SeatRequired = "Validation.Ticket.Seat.Required";
+    internal const string InvalidShow = "Validation.Ticket.Show.Invalid";
+}
